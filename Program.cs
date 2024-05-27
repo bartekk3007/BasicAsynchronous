f976534e3cfa87e75cf5f2c4fb8b9bcccb4b7684
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-05-27 23:05:06 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace AsynchronousTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int repetitions = 100;
+            // Aby zastosować bibliotekę TPL w wersjach starszych niż .NET 4.5, 
+            // wywołaj metodę Task.Factory.StartNew<string>().
+            Task task = Task.Run(() =>
+            {
+                for (int count = 0; count < repetitions; count++)
+                {
+                    Console.Write('-');
+                }
+            });
+            for (int count = 0; count < repetitions; count++)
+            {
+                Console.Write('+');
+            }
+
+            // Oczekiwanie na ukończenie zadania
+            task.Wait();
+        }
+    }
+}
